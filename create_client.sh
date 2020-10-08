8e4092c84b820abeb3ff07b0d38660355c4b532b
moom0o <48740106+moom0o@users.noreply.github.com>
2020-10-08 00:10:10 -0400

    Add files via upload

--- create_client.sh
+++ create_client.sh
@@ -2,24 +2,24 @@ new_client () {
 	# Generates the custom client.ovpn
 	{
 	cat /etc/openvpn/server/client-common.txt
-	echo "<ca>"
+	#echo "<ca>"
 	cat /etc/openvpn/server/easy-rsa/pki/ca.crt
-	echo "</ca>"
-	echo "<cert>"
+	#echo "</ca>"
+	#echo "<cert>"
 	sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/server/easy-rsa/pki/issued/"$client".crt
-	echo "</cert>"
-	echo "<key>"
+	#echo "</cert>"
+	#echo "<key>"
 	cat /etc/openvpn/server/easy-rsa/pki/private/"$client".key
-	echo "</key>"
-	echo "<tls-crypt>"
+	#echo "</key>"
+	#echo "<tls-crypt>"
 	sed -ne '/BEGIN OpenVPN Static key/,$ p' /etc/openvpn/server/tc.key
-	echo "</tls-crypt>"
-	} > ~/"$client".ovpn
+	#echo "</tls-crypt>"
+	} > /home/moomoo/"$client".ovpn
 }
 name=1
 client=$(sed 's/[^0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-]/_/g' <<< "$name")
 while [[ -z "$client" || -e /etc/openvpn/server/easy-rsa/pki/issued/"$client".crt ]]; do
-	echo "$client: invalid name."
+	#echo "$client: invalid name."
 	name=$((name + 1))
 	client=$(sed 's/[^0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-]/_/g' <<< "$name")
 done
@@ -27,7 +27,8 @@ cd /etc/openvpn/server/easy-rsa/
 EASYRSA_CERT_EXPIRE=3650 ./easyrsa build-client-full "$client" nopass
 # Generates the custom client.ovpn
 new_client
-echo
-echo "$client added. Configuration available in:" ~/"$client.ovpn"
+#echo
+#echo "$client added. Configuration available in:" /home/moomoo/"$client.ovpn"
+echo "$client"
 	exit
 	;;
